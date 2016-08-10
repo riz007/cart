5055b67c5721a469a7d7bdffb27f8620ca1ec2f0
bamma <bamma1985@gmail.com>
2016-08-10 11:09:00 +0700

    Add remove and checkout works

--- js/cart.js
+++ js/cart.js
@@ -11,9 +11,21 @@ $(document).ready(function() {
 	});
 
 	$('#cartContainer').on('change','#cart-amount', function () {
-   	console.log($(this).val());
+		var id = $(this).attr("data-id");
+		var cartData =getCartData();
+		 var newQuantity = $(this).val();
+		var result =	$.grep(cartData.items, function(e){ return e.id == id; });
+		var everythingElse =	$.grep(cartData.items, function(e){ return e.id != id; });
+		result[0].quantity  = newQuantity;
+		cartData.items = everythingElse;
+		cartData.items.push(result[0]);
+		setCartDate(cartData);
 	});
-	$('#cart-checkout').click(function(event){
+	$("#cartContainer").on('click','#remove-cart-item',function(){
+		var id = $(this).attr("data-id");
+		removeFromCart(id);
+	});
+	$('#cartContainer').on('click','#cart-checkout',function(event){
     	event.preventDefault();
     	var newForm = $('<form>', {
        	 	'action': 'https://www.paypal.com/cgi-bin/webscr',
@@ -23,7 +35,7 @@ $(document).ready(function() {
 		newForm = addHiddenInput(newForm,"upload","1");
 		newForm = addHiddenInput(newForm,"business","youremail@mail.com");
 		newForm = addHiddenInput(newForm,"currency_code","US");
-		var cartData =	JSON.parse(localStorage.getItem("cartContainer"));
+		var cartData =	getCartData();
 		console.log(cartData);
 		var index = 1;
 		cartData.items.forEach(function(element) {
@@ -43,69 +55,64 @@ function addHiddenInput(target,name, value){
 		'value' : value
 	}));
 };
-function addToCart(id,item,amount,price){
-	var container = $("#cartContainer")
-	var cartItem = $("#cartContainer").find("#"+id);
-	console.log(cartItem)
-	if(cartItem.length!=0)
+function addToCart(id,item,quantity,amount){
+	var cartData =	getCartData();
+	if(!cartData)
 	{
-		console.log("found container");
-		removeFromCart(id);
+		cartData = {
+			items : []
+		};
 	}
-	else
-	{
+	var result = $.grep(cartData.items, function(e){ return e.id == id; });
 		
-		console.log("did not find container");
-		var newItem = '<div id="' + id + '"><input id="cart-amount" type="number" value="'+amount+'"/>: '+ item +'<button id="remove-cart-item">remove</button></div>';
-		$("#cartContainer").prepend(newItem);
-		var cartData =	jQuery.data('#cartContainer','cartData');
-		
-		if(!cartData){
-			cartData = {
-				items : []
-			}
-		}
-		console.log(cartData);
-
-		var result = $.grep(cartData.items, function(e){ return e.id == id; });
-		
-		if(result.length == 0){// not found
-			console.log("no id found");
-			cartData.items.push({
-				id : id,
-				label : item,
-				quantity : amount,
-				amount : price
-			});
-		} 
-		else{ // found update data
-			result[0].amount+=amount;
-
-		}
-		localStorage.setItem("cartContainer",JSON.stringify(cartData));
+	if(result.length == 0){// not found
+		console.log("no id found");
+		cartData.items.push({
+			id : id,
+			label : item,
+			quantity : quantity,
+			amount : amount
+		});
+	} 
+	else{ // found update data
+		var everythingElse =	$.grep(cartData.items, function(e){ return e.id != id; });
+		result[0].quantity = +result[0].quantity +  +quantity;
+		cartData.items = everythingElse;
+		cartData.items.push(result[0]);
 	}
-	
+	setCartDate(cartData);
+	redrawCart();
 };
 function redrawCart(){
 	var container = $("#cartContainer")
 	container.empty();
-		var cartData =	JSON.parse(localStorage.getItem("cartContainer"));
-		cartData.items.forEach(function(element) {
-			var cartItem = '<div id="' + id + '"><input id="cart-amount" type="number" value="'+element.quantity+'"/>: '+ element.label +'<button id="remove-cart-item">remove</button></div>';
+		var cartData =	getCartData();
+		if(cartData){
+			cartData.items.sort(function(a,b){
+				var aName = a.label.toLowerCase();
+  				var bName = b.label.toLowerCase(); 
+  				return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
+			});
+			cartData.items.forEach(function(element) {
+			var cartItem = '<div><input id="cart-amount" type="number" value="'+element.quantity +'" data-id="'+ element.id +'"/>'
+				+ element.label 
+				+'<button id="remove-cart-item" data-id="'+ element.id +'">remove</button></div>';
 			container.append(cartItem);
 		}, this);
+		}
 	container.append('<button id="cart-checkout">Checkout</button>');
-}
-function storeData(cartItem){
-	
-}
+};
 function removeFromCart(id){
-	var container = $("#cartContainer").find("#"+id);
-	$(container).remove();
-}
-function CartItem(id,label,amount,quantity){
-	this.id=id;
-	this.quantity=quantity;
-	this.label=label;
-	this.amount=amount;
+
+	var cartData =	getCartData();
+	var everythingElse =	$.grep(cartData.items, function(e){ return e.id != id; });
+	cartData.items = everythingElse;
+	setCartDate(cartData);
+	redrawCart();
+};
+function getCartData(){
+	return 	JSON.parse(localStorage.getItem("cartContainer"));
+};
+function setCartDate(cartData){
+	localStorage.setItem("cartContainer",JSON.stringify(cartData));
 };
